1813d3b240408d39e6b9e6c6c0777191cc9ddac2
HimaBindhu-Madhamanchi <himabindhumadhamanchi@gmail.com>
2020-10-30 13:56:31 +0530

    Added ensure . and validate the mandatory 3rd part

--- emailverification.sh
+++ emailverification.sh
@@ -4,7 +4,7 @@ echo "Welcome to email verification program"
 
 read -p "Enter the Email-id:" email
 
-emailPat="^([A-Za-z0-9]+)?@[a-zA-Z]+"
+emailPat="^([A-Za-z0-9]+)?@[a-zA-Z]+.[a-zA-Z]{2,4}"
 
 if [[ $email =~ $emailPat ]]
 then
